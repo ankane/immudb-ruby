b59ff7116b5a39e03b3e8890f25351ab8b42af0a
Andrew Kane <andrew@ankane.org>
2022-06-12 19:43:38 -0700

    Improved SQL tests

--- test/sql_test.rb
+++ test/sql_test.rb
@@ -6,13 +6,14 @@ class SqlTest < Minitest::Test
 
     immudb.sql_exec("CREATE TABLE #{table} (id INTEGER, name VARCHAR, PRIMARY KEY id)")
     assert_includes immudb.list_tables, table
-    assert_equal ["id", "name"], immudb.describe_table(table).map { |v| v["COLUMN"] }
+    # sort needed for immudb 1.0
+    assert_equal ["id", "name"], immudb.describe_table(table).map { |v| v["COLUMN"] }.sort
 
     immudb.sql_exec("INSERT INTO #{table} (id, name) VALUES (1, 'Chicago')")
     immudb.sql_exec("INSERT INTO #{table} (id, name) VALUES (@id, @name)", {id: 2, name: "New York"})
     immudb.sql_exec("INSERT INTO #{table} (id, name) VALUES (@id, @name)", {id: 3, name: nil})
 
-    result = immudb.sql_query("SELECT * FROM #{table}")
+    result = immudb.sql_query("SELECT * FROM #{table} ORDER BY id")
     expected = [
       {"id" => 1, "name" => "Chicago"},
       {"id" => 2, "name" => "New York"},
